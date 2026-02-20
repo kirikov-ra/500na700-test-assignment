94d9c3f6da845fb7a1c48d62f0fbc0c4345de668
Kirikov Roman <vetproginc@gmail.com>
2026-02-20 12:40:18 +0300

    chore: configure global styles and SCSS support

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
+import './src/app/styles/reset.scss';
+import './src/app/styles/global.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './src/app/styles/index.scss'
 import App from './src/app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
